9824a2e49bec0985bdb8b710f18ab705fee379ea
Nopporn-Raffy <94434737+Nopporn-Raffy@users.noreply.github.com>
2022-02-19 16:25:21 +0700

    finish to connect API

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useStare, useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [profile, setProfile] = useState([]);
+  useEffect(()=>{
+    async function getALLProfile(){
+      try {
+        const profile = await axios.get("http://127.0.0.1:8000/api/profile")
+        console.log(profile.data)
+        setProfile(profile.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getALLProfile();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Connect API Laravel</h1>
+     {
+       profile.map((profile, i)=>{
+         return(
+           <h2>
+             {profile.name}
+             {profile.nickname}
+             {profile.email}
+           </h2>
+         );
+       })
+     }
     </div>
   );
 }
